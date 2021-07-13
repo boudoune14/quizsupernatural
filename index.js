2aff73090c2b9242bf4a1b01d24abb0baaba5266
Bauer <bauervicky1@gmail.com>
2021-07-13 23:07:11 +0200

    premier ajout

---
+++ index.js
@@ -0,0 +1,92 @@
+class Question {
+    constructor(text, choices, answer) {
+      this.text = text;
+      this.choices = choices;
+      this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+      return this.answer === choice;
+    }
+  }
+  let questions = [
+    new Question("Quelle est la premiere chasse de Sam et Dean ?", ["un vampire", "une dame blanche", "un clown malefique", "un wendigo"], "une dame blanche"),
+    new Question("De quelle marque est la voiture de Dean ?", ["Buick","Chevrolet Impala", "Cadillac", "GMC"], "Chevrolet Impala"),
+    new Question("Comment s'appelle la petite amie de Sam ?", ["Mary","Jo", "Jessica", "Meg"], "Jessica"),
+    new Question("Comment s'appelle le démon qui a tué leur mére ?", ["Azazel","Crowley", "Lucifer", "Lucifer"], "Math.round()")
+  ];
+  
+  console.log(questions);
+  
+  class Quiz {
+    constructor(questions) {
+      this.score = 0;
+      this.questions = questions;
+      this.currentQuestionIndex = 0;
+    }
+    getCurrentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    }
+    guess(answer) {
+      if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+      }
+      this.currentQuestionIndex++;
+    }
+    hasEnded() {
+      return this.currentQuestionIndex >= this.questions.length;
+    }
+  }
+  
+  // Regroup all  functions relative to the App Display
+  const display = {
+    elementShown: function(id, text) {
+      let element = document.getElementById(id);
+      element.innerHTML = text;
+    },
+    endQuiz: function() {
+      endQuizHTML = `
+        <h1>Quiz terminé !</h1>
+        <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
+      this.elementShown("quiz", endQuizHTML);
+    },
+    question: function() {
+      this.elementShown("question", quiz.getCurrentQuestion().text);
+    },
+    choices: function() {
+      let choices = quiz.getCurrentQuestion().choices;
+  
+      guessHandler = (id, guess) => {
+        document.getElementById(id).onclick = function() {
+          quiz.guess(guess);
+          quizApp();
+        }
+      }
+      // display choices and handle guess
+      for(let i = 0; i < choices.length; i++) {
+        this.elementShown("choice" + i, choices[i]);
+        guessHandler("guess" + i, choices[i]);
+      }
+    },
+    progress: function() {
+      let currentQuestionNumber = quiz.currentQuestionIndex + 1;
+      this.elementShown("progress", "Question " + currentQuestionNumber + " sur " + quiz.questions.length);
+    },
+  };
+  
+  // Game logic
+  quizApp = () => {
+    if (quiz.hasEnded()) {
+      display.endQuiz();
+    } else {
+      display.question();
+      display.choices();
+      display.progress();
+    } 
+  }
+  // Create Quiz
+  let quiz = new Quiz(questions);
+  quizApp();
+  
+  console.log(quiz);
+  
+  
